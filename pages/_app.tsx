7d79426ef1d04f17ef6c2302c176b808e1a8bfd8
PATEL96 <patel.raj.official.96@gmail.com>
2024-10-06 11:45:59 +0530

    feat: UI reverted..

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
 import Web3Provider from '../Web3Provider/Web3Provider';
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
-import { Dashboard } from './dashboard';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Web3Provider>
-        <Dashboard />
+        <Component {...pageProps} />
       </Web3Provider>
     </>
   );
