e7ee846353df67987a350cb3e577086ca5615c8f
FlayerDrift01 <karmvirsinhgohil451@gmail.com>
2024-10-06 11:27:51 +0530

    (dashboard)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import Web3Provider from '../Web3Provider/Web3Provider';
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
+import { Dashboard } from './dashboard';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Web3Provider>
-        <Component {...pageProps} />
+        <Dashboard />
       </Web3Provider>
     </>
   );
